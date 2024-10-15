9d55c7bb3cee6d5fff959170b0e8ebe5b225fc33
Arsh-Gandhewar <145112815+Arsh-Gandhewar@users.noreply.github.com>
2024-10-15 22:14:07 +0530

    Add files via upload

---
+++ HCI/app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('signUpButton'); 
+
+button.addEventListener('click', () => {
+ 
+  window.location.href = 'C:/Users/arshg/OneDrive/Desktop/HCI/login.html';
+});
